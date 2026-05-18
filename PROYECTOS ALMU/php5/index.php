436b9cfe2524397e86346946296220b885683c7c
AlmudenaRomero <almudena.romero.tn@gmail.com>
2026-05-18 13:59:57 +0200

    Update index.php

--- PROYECTOS ALMU/php5/index.php
+++ PROYECTOS ALMU/php5/index.php
@@ -2,11 +2,17 @@
   $my_String="Cadena de texto";
   echo $my_String . "\n";
 
-  $my_String=6;
+  //variables dinamicas, cambian con la asignación 
+  $my_String=6; 
   echo gettype ($my_String);
 
+ //problemas al operar con diferentes variables, pero int y float se lo come
+ 
+ $my_int=7;
+ $my_float=4.5;
+ $muy_int =$my_int + $my_float;
 
-   
-
+ echo $my_int. "\n";
+ echo gettype($my_float). "\n";
 
 ?>
